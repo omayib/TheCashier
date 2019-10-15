d1a43f3e044c48d6762ab201d0f8ea49544a2027
omayib <omayib@gmail.com>
2019-10-15 18:32:50 +0700

    the cashier showing up

---
+++ TheCompany/ItemCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCompany
+{
+    abstract class ItemCalculator
+    {
+        protected Item item { get; private set; }
+        public ItemCalculator(Item item)
+        {
+            this.item = item;
+        }
+        public abstract double calculateSubTotal();
+    }
+}
